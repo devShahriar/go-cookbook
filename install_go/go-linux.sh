468ae5204707f4160db51fde29b3a90e59a097d1
S M Shahriar Islam <56567436+devShahriar@users.noreply.github.com>
2021-10-16 19:53:44 +0600

    Update go-linux.sh

--- install_go/go-linux.sh
+++ install_go/go-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -c https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
+sudo wget -c https://golang.org/dl/go1.15.2.linux-amd64.tar.gz
 
 sudo tar -C /usr/local -xzf go1.15.2.linux-amd64.tar.gz
 
